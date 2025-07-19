50030285e7d0330f7489dcfb30508ae2a0f85851
Øyvind Solheim <oyvindbs@gmail.com>
2025-07-19 21:46:23 +0000

    lacked rhe log utility

--- app/src/main/java/oyvindbs/zotshelf/MainActivity.java
+++ app/src/main/java/oyvindbs/zotshelf/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.app.AlertDialog;
